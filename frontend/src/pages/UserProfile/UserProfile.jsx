9f5a6308a8e67a6dbc75f39d8c999cfc97fa067e
Carol Wargo <carolwargo@gmail.com>
2025-04-21 09:28:34 -0400

    update

--- frontend/src/pages/UserProfile/UserProfile.jsx
+++ frontend/src/pages/UserProfile/UserProfile.jsx
@@ -1,12 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import GirlCamera from "../../assets/images/GirlCamera.png";
-import ResumeDownload from "../../assets/ResumeDownload.pdf";
-import { FaGoogle } from "react-icons/fa";
-import { FaMicrosoft } from "react-icons/fa"; // Font Awesome
-import { SiAdobecreativecloud } from "react-icons/si"; // Font Awesome
 import "bootstrap/dist/css/bootstrap.min.css";
-import UserProfile from "../../components/ProfileComponents/UserProfile";
+import UserProfile from "../../components/ProfileComponents/UserProfile/UserProfile";
 
 export default function ResumePage() {
   return (
